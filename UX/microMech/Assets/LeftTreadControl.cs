5b5ec368d1358eb68c9374d5854552a422b2511a
Imran Peerbhai <imran@thinkpredict.com>
2019-06-23 12:40:06 -0700

    Micromech3 has controller joystick reads working.

---
+++ UX/microMech/Assets/LeftTreadControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftTreadControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] joysticks =  UnityEngine.Input.GetJoystickNames();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // there are two joysticks -- a left and a right.  Each joystick has multiple axes.  I want to use the left thumbstick vertical axis
+        // for the left controller.
+        var movedY = UnityEngine.Input.GetAxis("LeftThumbstickVertical");
+
+
+    }
+}
